5190d948873523ec7ac08f740a7a0a795634a2ad
motuzj <30744041+motuzj@users.noreply.github.com>
2023-03-04 20:40:13 +0100

    lifestyle, favicon, socials, menu, logo
    Former-commit-id: 16c9ad23eba44cb80ca054615841241381e7f368

--- js/navbar.js
+++ js/navbar.js
@@ -1,8 +1,20 @@
-function myFunction() {
+function showMenu() {
     var x = document.getElementById("links");
+    var y = document.getElementById("kontakt2");
+    var z = document.getElementById("kontakt1");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
+    }
+    if (y.style.display === "contents") {
+        y.style.display = "none";
+    } else {
+        y.style.display = "contents";
+    }
+    if (z.style.display === "flex") {
+        z.style.display = "none";
+    } else {
+        z.style.display = "flex";
     }
 }
